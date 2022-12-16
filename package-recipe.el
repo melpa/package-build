c801bd2f109cd186d0b01c787ef761abe3c17770
Jonas Bernoulli <jonas@bernoul.li>
2022-12-16 01:44:18 +0100

    package-recipe--validate: Disallow "*" in :files
    (file-expand-wildcards "*") also matches files beginning with a
    period, but we obviously don't want to include ".git" in archives.

--- package-recipe.el
+++ package-recipe.el
@@ -198,11 +198,15 @@ file is invalid, then raise an error."
         ;; All other elements have to be strings or lists of strings.
         ;; A list whose first element is `:exclude' is also valid.
         (dolist (entry spec)
-          (unless (or (stringp entry)
+          (unless (or (and (stringp entry)
+                           (not (equal entry "*")))
                       (and (listp entry)
                            (or (eq (car entry) :exclude)
                                (stringp (car entry)))
-                           (seq-every-p #'stringp (cdr entry))))
+                           (seq-every-p (lambda (e)
+                                          (and (stringp e)
+                                               (not (equal e "*"))))
+                                        (cdr entry))))
             (error "Invalid files spec entry %S" entry))))
       ;; Silence byte compiler of Emacs 28.  It appears that uses
       ;; inside cl-assert sometimes, but not always, do not count.
