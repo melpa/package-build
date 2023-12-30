abad0d94a5e79d3c39b0f89ce2aac02053d0f6a5
Jonas Bernoulli <jonas@bernoul.li>
2023-12-30 18:39:56 +0100

    Silence byte-compiler
    This completes [1: 1de4f6c].
    
    1: 2023-12-23 1de4f6cea15838084776abdf1858b536287cb872
       Account for Emacs 30's relocation of package--prepare-dependencies

--- package-build.el
+++ package-build.el
@@ -1085,12 +1085,15 @@ value specified in the file \"NAME.el\"."
 ^;;; [^ ]*\\.el ---[ \t]*\\(.*?\\)[ \t]*\\(-\\*-.*-\\*-[ \t]*\\)?$" nil t)
                        (match-string-no-properties 1)))
                 "No description available.")
-            (if (fboundp 'lm-package-requires)
-                (lm-package-requires)
-              (when-let ((require-lines (lm-header-multiline "package-requires")))
+            (cond
+             ((fboundp 'lm-package-requires)
+              (lm-package-requires))
+             ((fboundp 'package--prepare-dependencies)
+              (and-let* ((require-lines
+                          (lm-header-multiline "package-requires")))
                 (package--prepare-dependencies
                  (package-read-from-string
-                  (mapconcat #'identity require-lines " ")))))
+                  (mapconcat #'identity require-lines " "))))))
             ;; `:kind' and `:archive' are handled separately.
             :kind       (or kind 'single)
             ;; The other keyword arguments are appended to the alist
