3759645685e1706a8db60104aeb36ed869691f0d
Jonas Bernoulli <jonas@bernoul.li>
2026-07-01 12:39:22 +0200

    Mention MyELPA in documentation

--- package-build.el
+++ package-build.el
@@ -38,6 +38,10 @@
 ;; describe Emacs Lisp projects and repositories from which to get
 ;; them.
 
+;; This package is used by, and maintained as part of, MELPA.
+;; You can also use it to create your own ELPA* with little effort,
+;; see https://codeberg.org/tarsius/myelpa/wiki.
+
 ;;; Code:
 
 (require 'cl-lib)
